77003d5c125865f20311d9ee66075b23e0bbf2d9
Kadin Sayani <me@kadinsayani.dev>
2023-09-12 22:06:15 -0600

    updated image names

--- Kanswift/Views/CardView.swift
+++ Kanswift/Views/CardView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct CardView: View {
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 5).stroke(.blue, lineWidth: 1).frame(width: 150, height: 150)
+            RoundedRectangle(cornerRadius: 5).stroke(.blue, lineWidth: 1).frame(width: 150, height: 100)
         }
     }
 }
